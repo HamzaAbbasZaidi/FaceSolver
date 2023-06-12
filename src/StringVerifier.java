1f1886fb7ccb3f74e98a8669aec77f2e23858cf2
hamzazaidi <hamza.zaidi21@ic.ac.uk>
2023-06-12 02:55:14 +0100

    Initial commit

---
+++ src/StringVerifier.java
@@ -0,0 +1,16 @@
+public class StringVerifier {
+
+    public static boolean verifyString(String input) {
+        if (input.length() != 9) {
+            return false;
+        }
+
+        for (char c : input.toCharArray()) {
+            if (c != 'w' && c != 'b' && c != 'g' && c != 'r' && c != 'y' && c != 'o') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
